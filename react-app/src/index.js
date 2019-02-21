ee2e9f24483162de31420aaba178bd797ab00510
MichaelJahns <jahnsmic001@gmail.com>
2019-02-21 13:02:01 -0800

    Hello World Achieved

---
+++ react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1> I exist. </h1>;
+
+console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
